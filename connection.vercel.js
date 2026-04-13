f2d265de4351e0b4fe90f8778fcf079401cdb7e7
Adri . <javier.adrian.molina.paredes@Gmail.com>
2026-04-13 14:38:05 -0400

    otros cambios

--- connection.vercel.js
+++
@@ -1,8 +0,0 @@
-import { Sandbox } from "@vercel/sandbox";
-
-const test = await Sandbox.create();
-
-const cmd = await Sandbox.runCommand("echo", ["Hello desde vercelStudio"]);
-console.log(await cmd.stdout());
-
-await test.stop();
