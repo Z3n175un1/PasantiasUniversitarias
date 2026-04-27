d97dc174eeccf5e9daa0bf2ed0aa722f7b690ff1
Adri . <javier.adrian.molina.paredes@Gmail.com>
2026-04-27 14:26:05 -0400

    Php mi legion no la grasa xd

--- database/seeders/adminseeder.php
+++ database/seeders/adminseeder.php
@@ -17,12 +17,18 @@ class adminseeder extends Seeder
         DB::table('users')->insert([
             'name' => 'Adridmin',
             'email' => [EMAIL_ADDRESS],
+            'email_verified_at' => now(),
+            'created_at' => now(),
+            'updated_at' => now(),
             'password' => Hash::make('adridmin123'),
             'role' => 'admin'
         ]);
         DB::table('users')->insert([
             'name' => 'Fabridmin',
             'email' => [EMAIL_ADDRESS],
+            'email_verified_at' => now(),
+            'created_at' => now(),
+            'updated_at' => now(),
             'password' => Hash::make('fabri123'),
             'role' => 'admin'
         ]);
