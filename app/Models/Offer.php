2a810418a50207a68b7ed0a54c1bab880dbe83bc
Adri . <javier.adrian.molina.paredes@Gmail.com>
2026-05-04 13:52:05 -0400

    cambios

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -5,6 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Offer extends Model
-{
-    //
+{  
+    protected table $ofertas;
+    protected $fillable = [
+        'titulo',
+        'tipo',
+        'duracion',
+        'carrera'
+    ];
+    public function ofertas():belongsTo{
+        return ofertas;
+    }
 }
