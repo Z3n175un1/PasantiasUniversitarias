3228ab9278c8e2631b23c6200d84820501f95bb5
Fabricio <fabricio@debian.debian>
2026-05-04 01:45:36 -0400

    agregado de tabla usuarios a la base de datos

---
+++ app/Models/Usuario.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    protected $table = 'usuarios';
+
+    protected $fillable = [
+        'nombre',
+        'correo',
+        'contrasena',
+    ];
+
+    protected $hidden = [
+        'contrasena',
+        'remember_token',
+    ];
+}
