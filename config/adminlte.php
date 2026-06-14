45868ea4ae00f04a1ff30311e892bfca8828679c
Adri . <javier.adrian.molina.paredes@Gmail.com>
2026-06-13 22:48:09 -0400

    edit: modificaciones de iconos para el sidebar del admin

--- config/adminlte.php
+++ config/adminlte.php
@@ -64,8 +64,8 @@ return [
     */
 
     'logo' => '<b>UWork</b>Flow',
-    'logo_img' => null,
-    'logo_img_class' => 'brand-image img-circle elevation-3',
+    'logo_img' => 'images/uworkflow-logo.svg',
+    'logo_img_class' => 'brand-image',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
     'logo_img_alt' => 'UWorkFlow Logo',
